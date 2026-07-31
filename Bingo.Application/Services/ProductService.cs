0544e4afa594d56a5c7e73795dbafe69f918dcdf
Mishel Shaji <33393350+mishelshaji@users.noreply.github.com>
2026-07-31 13:59:45 +0530

    Created product controller and validator

---
+++ Bingo.Application/Services/ProductService.cs
@@ -0,0 +1,6 @@
+﻿namespace Bingo.Application.Services;
+
+public class ProductService
+{
+    
+}
---
+++ Bingo.WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,13 @@
+﻿using Bingo.Application.Dtos.Product;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bingo.WebApi.Controllers;
+
+public class ProductsController: ApplicationControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> CreateAsync([FromBody]ProductCreateDto dto, CancellationToken cancellationToken)
+    {
+        return Ok(dto);
+    }
+}
